1769e14a90674b3af6018ac64f8954e6b43624f4
Leandro Soares <leandro.moreira@dbmaster.com.br>
2018-03-22 17:43:22 -0300

    modal

--- Tarsus/src/app/navbar/navbar.component.ts
+++ Tarsus/src/app/navbar/navbar.component.ts
@@ -5,6 +5,7 @@ import { Login } from '../model/login';
 import { Token } from '../model/token';
 import { UsuarioService } from '../usuario.service';
 import { Erro } from '../model/erro';
+import { Mensagem} from '../model/Mensagem';
 
 @Component({
   selector: 'app-root',
@@ -14,20 +15,34 @@ import { Erro } from '../model/erro';
 export class NavbarComponent {
   login: Login;
   logado: Boolean;
+  mensagem: Mensagem;
   
   
   constructor(
     public usuarioService : UsuarioService
   ) {
     this.login = new Login();
-   
+    this.mensagem = new Mensagem();
   }
 
   ngOnInit() {
+    
   }
 
   public entrar(): void{
     this.usuarioService.entrar(this.login);
     this.login = new Login();
+  }
+
+  public showModal(mensagem: Mensagem): void{
+    this.mensagem.classType = 'ERRO';
+    this.mensagem.tipo = 'Erro - mensagem de erro';
+    this.mensagem.texto = 'Teste de mensagem de alerta para erro';
+    var modal = document.getElementById('myModal');
+    modal.style.display = "block";
+  }
+  public closeModal(): void{
+    var modal = document.getElementById('myModal');
+    modal.style.display = "none";
   }
 }
